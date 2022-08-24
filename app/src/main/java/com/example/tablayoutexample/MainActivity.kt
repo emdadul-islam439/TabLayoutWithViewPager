fe0096af88b5bbcf7e13348d0a5d61ce54c87971
emdadul_islam <emdadulislam439@gmail.com>
2022-08-24 12:52:54 +0600

    very minimal code change to smoothly run with ViewPager 2

--- app/src/main/java/com/example/tablayoutexample/MainActivity.kt
+++ app/src/main/java/com/example/tablayoutexample/MainActivity.kt
@@ -5,13 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
-import androidx.viewpager.widget.ViewPager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
     private var toolbar: Toolbar? = null
-    private lateinit var viewPager: ViewPager
+    private lateinit var viewPager: ViewPager2
     private lateinit var tabLayout: TabLayout
     private var homeFragment: HomeFragment? = null
     private var settingsFragment: SettingsFragment? = null
@@ -30,11 +32,10 @@ class MainActivity : AppCompatActivity() {
         settingsFragment = SettingsFragment()
         chatsFragment = ChatsFragment()
 
-        tabLayout.setupWithViewPager(viewPager)
         //create viewpager adapter
         //here we will create inner class for adapter
         val viewPagerAdapter: ViewPagerAdapter = ViewPagerAdapter(
-            supportFragmentManager, 0
+            supportFragmentManager, this.lifecycle
         )
         //add fragments and set the adapter
         viewPagerAdapter.addFragment(homeFragment, "")
@@ -47,14 +48,21 @@ class MainActivity : AppCompatActivity() {
         tabLayout.getTabAt(1)?.setIcon(R.drawable.google_play)
         tabLayout.getTabAt(2)?.setIcon(R.drawable.heart)
 
+
+        //added in this position, because the app crashes if this portion is in the upper side
+//        tabLayout.setupWithViewPager(viewPager)
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = "OBJECT ${(position + 1)}"
+        }.attach()
+
         //set the badge
         val badgeDrawable = tabLayout.getTabAt(0)!!.orCreateBadge
         badgeDrawable.isVisible = true
         badgeDrawable.number = 5
     }
 
-    private inner class ViewPagerAdapter(fm: FragmentManager, behavior: Int) :
-        FragmentPagerAdapter(fm, behavior) {
+    private inner class ViewPagerAdapter(fm: FragmentManager, lifeCycle: Lifecycle) :
+        FragmentStateAdapter(fm, lifeCycle) {
         private val fragments: MutableList<Fragment> = ArrayList()
         private val fragmentTitles: MutableList<String> = ArrayList()
 
@@ -64,17 +72,25 @@ class MainActivity : AppCompatActivity() {
             fragmentTitles.add(title)
         }
 
-        override fun getItem(position: Int): Fragment {
-            return fragments[position]
-        }
+//        override fun getItem(position: Int): Fragment {
+//            return fragments[position]
+//        }
+//
+//        override fun getCount(): Int {
+//            return fragments.size
+//        }
+//
+//        //to setup title of the tab layout
+//        override fun getPageTitle(position: Int): CharSequence? {
+//            return fragmentTitles[position]
+//        }
 
-        override fun getCount(): Int {
+        override fun getItemCount(): Int {
             return fragments.size
         }
 
-        //to setup title of the tab layout
-        override fun getPageTitle(position: Int): CharSequence? {
-            return fragmentTitles[position]
+        override fun createFragment(position: Int): Fragment {
+            return fragments[position]
         }
     }
 }
